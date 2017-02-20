d93c66850fede164ce751a41638e2c1c477905a2
Alan Butler <rabutler@usbr.gov>
2017-02-20 09:43:52 -0700

    converted the query of noaa's site into a function

--- getData.R
+++ getData.R
@@ -1,26 +1,25 @@
 library(rnoaa)
-
 options(noaakey = Sys.getenv('NOAAAPIKEY'))
 
-tmp <- ncdc_locs(locationcategoryid='CITY', sortfield='name', sortorder='desc')$data
+#tmp <- ncdc_locs(locationcategoryid='CITY', sortfield='name', sortorder='desc')$data
 
-# boulder, colorado ID
-cityId <- 'GHCND:USC00050848'
+# cityId defaults to boulder, colorado ID
+# dataId could also be TMIN
+getTempsThroughToday <- function(day2, cityId = 'GHCND:USC00050848', dataId = 'TMAX')
+{
+  #out1 <- ncdc(datasetid='GHCND', stationid=cityId, datatypeid='PRCP', startdate = '2010-03-01', enddate = '2010-05-31', limit=500)
 
-out1 <- ncdc(datasetid='GHCND', stationid=cityId, datatypeid='PRCP', startdate = '2010-03-01', enddate = '2010-05-31', limit=500)
+  # average temp, max temp, min
+  # Per https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt, the units are tenths of
+  # degrees C
 
-# average temp, max temp, min
-# Per https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt, the units are tenths of
-# degrees C
-dataId <- c('TMAX','TMIN')
+  day1 <- '2017-01-01'
 
-day1 <- '2016-12-01'
-day2 <- '2016-12-31'
+  out2 <- ncdc(datasetid='GHCND', stationid=cityId, datatypeid=dataId, 
+               startdate = day1, enddate = day2, limit=365)
 
-out2 <- ncdc(datasetid='GHCND', stationid=cityId, datatypeid=dataId[1], 
-             startdate = day1, enddate = day2, limit=365)
+  # convert to degress F from tenths of degrees C
+  out2$data$value <- out2$data$value / 10 * 9/5 +32
 
-# convert to degress F from tenths of degrees C
-out2$data$value <- out2$data$value / 10 * 9/5 +32
-
-ncdc_stations(datasetid='GHCND', stationid='cityId')
+  out2    
+}
