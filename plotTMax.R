0b86a15587a8009c11f06885113706958858931f
Alan Butler <rabutler@usbr.gov>
2017-03-11 12:59:09 -0700

    updated to show the record highs and lows
    - joined the historical and current temp data frames
    - added in plotting of points on record highs and lows

--- plotTMax.R
+++ plotTMax.R
@@ -34,9 +34,19 @@ day2 <- as.character(Sys.Date() - 1)
 # record; probably combine with h2 so we don't have to use two different data frames
 cTmp <- getTempsThroughToday(day2)
 cTmp$data <- cTmp$data %>%
-  mutate(newDay = seq(1, length(date)))
+  mutate(newDay = seq(1, length(date))) %>%
+  select(newDay, value) %>%
+  rename(cTmp = value)
 
-yRange <- range(h2$tmax.max, h2$tmax.min, cTmp$data$value)
+# join the current data with the historical data
+h2 <- h2 %>%
+  left_join(cTmp$data, by = 'newDay') %>%
+  # if the current days high temp is a record high, or record low, set it to cTmp
+  mutate(recordHigh = ifelse(cTmp > tmax.max, cTmp, NA),
+         recordLow = ifelse(cTmp < tmax.min, cTmp, NA))
+
+# compute the y-range to use based on the data
+yRange <- range(h2$tmax.max, h2$tmax.min, h2$cTmp, na.rm = TRUE)
 yRange[1] <- floor(yRange[1] / 10) * 10 # round down to nearest 10
 yRange[2] <- ceiling(yRange[2] / 10) * 10 # round up to nearest 10
 yLabs <- seq(yRange[1], yRange[2],10)
@@ -54,12 +64,14 @@ ggplot(h2) +
         axis.line.y = element_line(color = 'wheat4', size = 1)) +
   geom_linerange(aes(x = newDay, ymin=tmax.min, ymax=tmax.max), color = "wheat2") +
   geom_linerange(aes(x=newDay, ymin=tmax.25, ymax=tmax.75), colour = "wheat4") +
-  geom_line(data = cTmp$data, aes(newDay, value), size = .75, color = 'grey40') +
+  geom_line(aes(newDay, cTmp), size = .75, color = 'grey40') +
   scale_x_continuous(expand = c(0,0), labels = month.name, 
                      breaks = c(15,45,75,105,135,165,195,228,258,288,320,350)) +
   coord_cartesian(ylim = range(yLabs)) +
-  scale_y_continuous(labels = yLabs, breaks = yLabs) +
+  scale_y_continuous(labels = dgr_fmt(yLabs), breaks = yLabs) +
   geom_hline(yintercept = yLabs, color = 'white') +
-  geom_vline(xintercept = eomDays, color = 'wheat4', linetype = 3, size = .5)
+  geom_vline(xintercept = eomDays, color = 'wheat4', linetype = 3, size = .5) +
+  geom_point(aes(x = newDay, y = recordHigh), color = 'firebrick3') +
+  geom_point(aes(x = newDay, y = recordLow), color = 'blue3')
 
   
