c21832a0da8ef4ffeee093ef7d31e3cc5821f944
Alan Butler <rabutler@usbr.gov>
2017-04-30 12:02:18 -0600

    batch output now goes to text file

--- updateTempFigure.bat
+++ updateTempFigure.bat
@@ -1,6 +1,8 @@
-cd C:\Users\Wheatgrass\Documents\GitHub\BoulderTemps\
-"C:\Program Files\R\R-3.3.1\bin\Rscript.exe" plotTMax.R
-"C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m "scheduled commit"
-"C:\Program Files\Git\bin\git.exe" push
-PAUSE
+@echo off
+>batchOutput.txt (
+	cd C:\Users\Wheatgrass\Documents\GitHub\BoulderTemps\
+	"C:\Program Files\R\R-3.3.1\bin\Rscript.exe" plotTMax.R
+	"C:\Program Files\Git\bin\git.exe" add .
+	"C:\Program Files\Git\bin\git.exe" commit -m "scheduled commit"
+	"C:\Program Files\Git\bin\git.exe" push
+)
