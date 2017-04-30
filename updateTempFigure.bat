6db080a3670c79ffe6a92fc67ce294474a1cbd4e
Alan Butler <rabutler@usbr.gov>
2017-04-30 11:32:09 -0600

    initial take at batch script

---
+++ updateTempFigure.bat
@@ -0,0 +1,7 @@
+cd C:\Users\Wheatgrass\Documents\GitHub\BoulderTemps\
+set
+"C:\Program Files\R\R-3.3.1\bin\Rscript.exe" plotTMax.R
+"C:\Program Files\Git\bin\git.exe" add -A
+"C:\Program Files\Git\bin\git.exe" commit -m 'scheduled commit'
+"C:\Program Files\Git\bin\git.exe" push
+PAUSE
