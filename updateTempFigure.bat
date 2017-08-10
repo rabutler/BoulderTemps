0b5f6f14c74ae64f434574be8f52ac78afa8ff6f
Alan Butler <r.alan.butler@gmail.com>
2017-08-09 21:32:12 -0600

    updates to batch script and plotting code for the new location on new laptop

--- updateTempFigure.bat
+++ updateTempFigure.bat
@@ -1,11 +1,11 @@
 @echo off
 >batchOutput.txt 2>&1(
-	cd C:\Users\Wheatgrass\Documents\GitHub\BoulderTemps\
-	"C:\Program Files\R\R-3.3.1\bin\Rscript.exe" plotTMax.R
+	cd C:\Users\Alan\Documents\projects\BoulderTemps\
+	"C:\Program Files\R\R-3.4.1\bin\Rscript.exe" plotTMax.R
 	"C:\Program Files\Git\bin\git.exe" add .
 	"C:\Program Files\Git\bin\git.exe" commit -m "scheduled commit"
 	"C:\Program Files\Git\bin\git.exe" push
-	cd C:\users\Wheatgrass\Documents\rabutler.github.io\
+	cd C:\Users\Alan\Documents\projects\site\
 	"C:\Program Files\Git\bin\git.exe" add .
 	"C:\Program Files\Git\bin\git.exe" commit -m "scheduled commit"
 	"C:\Program Files\Git\bin\git.exe" push
