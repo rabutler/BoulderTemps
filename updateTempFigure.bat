247bfdb9217f4c9e8cb81f9cedbaf93cce1030a6
Alan Butler <rbutler47@gatech.edu>
2020-02-19 22:21:41 -0700

    updated batch script

--- updateTempFigure.bat
+++ updateTempFigure.bat
@@ -5,7 +5,7 @@
 	"C:\Program Files\Git\bin\git.exe" add .
 	"C:\Program Files\Git\bin\git.exe" commit -m "scheduled commit"
 	"C:\Program Files\Git\bin\git.exe" push
-	cd C:\Users\Alan\Documents\projects\site\
+	cd C:\Users\Alan\Documents\projects\rabutler.github.io
 	"C:\Program Files\Git\bin\git.exe" add .
 	"C:\Program Files\Git\bin\git.exe" commit -m "scheduled commit"
 	"C:\Program Files\Git\bin\git.exe" push
