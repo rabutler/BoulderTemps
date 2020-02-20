745dee9b89c05c7684b73aac22fb38fe7351b1ea
Alan Butler <rbutler47@gatech.edu>
2020-02-19 22:17:26 -0700

    scheduled commit

--- updateTempFigure.bat
+++ updateTempFigure.bat
@@ -1,7 +1,7 @@
 @echo off
 >batchOutput.txt 2>&1(
-	cd C:\Users\Alan\Documents\projects\BoulderTemps\
-	"C:\Program Files\R\R-3.5.1\bin\Rscript.exe" plotTMax.R
+	cd C:\Users\Alan\Documents\projects\BoulderTemps
+	"Rscript.exe" main.R
 	"C:\Program Files\Git\bin\git.exe" add .
 	"C:\Program Files\Git\bin\git.exe" commit -m "scheduled commit"
 	"C:\Program Files\Git\bin\git.exe" push
